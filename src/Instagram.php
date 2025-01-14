2ac0c0ff3c526ebe6d45bec7a1c099d4fdc77329
Jade Geels <a@jgw.tf>
2025-01-14 11:43:38 +0100

    Use const for API version

--- src/Instagram.php
+++ src/Instagram.php
@@ -8,8 +8,9 @@ use Illuminate\Support\Facades\Config;
 
 class Instagram
 {
+    const API_VERSION = 'v21.0';
     const REQUEST_ACCESS_TOKEN_URL = "https://api.instagram.com/oauth/access_token";
-    const GRAPH_USER_INFO_FORMAT = "https://graph.instagram.com/v21.0/me?fields=id,media_count,username&access_token=%s";
+    const GRAPH_USER_INFO_FORMAT = "https://graph.instagram.com/%s/me?fields=id,media_count,username&access_token=%s";
     const EXCHANGE_TOKEN_FORMAT = "https://graph.instagram.com/access_token?grant_type=ig_exchange_token&client_secret=%s&access_token=%s";
     const REFRESH_TOKEN_FORMAT = "https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=%s";
     const MEDIA_URL_FORMAT = "https://graph.instagram.com/%s/media?fields=%s&limit=%s&access_token=%s";
@@ -71,7 +72,7 @@ class Instagram
 
     public function fetchUserDetails($access_token)
     {
-        $url = sprintf(self::GRAPH_USER_INFO_FORMAT, $access_token['access_token'] ?? $access_token['access_code']);
+        $url = sprintf(self::GRAPH_USER_INFO_FORMAT, self::API_VERSION, $access_token['access_token'] ?? $access_token['access_code']);
         return SimpleClient::get($url);
     }
 
